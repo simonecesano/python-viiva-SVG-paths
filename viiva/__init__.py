8081e874033b4f9e75f3457737b8792d87689d62
Simone Cesano <simonecesano@libero.it>
2024-08-23 16:47:52 +0200

    Factored Paths out, changed into package

--- viiva/__init__.py
+++ viiva/__init__.py
@@ -1,5 +1,6 @@
 from .beziers import BezierPath
-from .paths import CubicBezier, Arc, Line, QuadraticBezier, Path
+from .paths import CubicBezier, Arc, Line, QuadraticBezier
+from .paths.path import Path
 
 __all__ = [
     'CubicBezier',
